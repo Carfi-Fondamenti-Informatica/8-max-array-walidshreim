65fe5fc199f1ea9a198e267a410bee096f5000a0
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-12 22:38:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+float numeromassimo( const float numeri[], int dim){
+  numeri [dim];
+  float massimo = numeri[0];
+  for(int i=0; i<dim; i++){
+    if(massimo< numeri[i]){
+      massimo = numeri[i];
+    }
+  }
+  return massimo;
+}
