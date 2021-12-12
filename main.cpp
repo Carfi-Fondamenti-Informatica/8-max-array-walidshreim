72429b78248357ccfd842defa1e7a3910a129d94
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-12 22:41:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int dim;
+  cin>> dim;
+  float numeri[dim];
+  for (int i=0; i<dim; i++){
+    cin>>  numeri[i];
+  }
+  numeromassimo(numeri, dim);
+  cout<< numeromassimo(numeri, dim)<< endl;
+  
+  
   return 0;
 }
